9bdda0670ff05d00a2863207c23f49f4a03d779b
Robin Vobruba <hoijui.quaero@gmail.com>
2024-05-19 11:45:45 +0100

    setup.py: State minimum required Python version as 3.7

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Operating System :: OS Independent",
         "Natural Language :: English",
     ],
+    python_requires='>=3.7',
     tests_require=["pytest"],
     test_suite="tests",
 )
